c8f44a224b754b12606da9715f6ff80df4b102a5
SergDv1962 <dsv19621227@gmail.com>
2024-07-21 15:14:10 +0300

    налаштування та запуск серверу та перевірка запитів через постман

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /api/user/login
+router.post('/login', function(req, res) {
+  res.send('login');
+});
+// /api/user/register
+router.post('/register', function(req, res) {
+  res.send('register');
+});
+// /api/user/current
+router.get('/current', function(req, res) {
+  res.send('current');
 });
 
 module.exports = router;
