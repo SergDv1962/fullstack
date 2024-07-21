3d642d746dd64bbff88e4dd28e11173f1579d1da
SergDv1962 <dsv19621227@gmail.com>
2024-07-21 17:06:59 +0300

    додали папку controllers

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { login, register, current } = require('../controllers/users.js');
+const router = express.Router();
 
 // /api/user/login
-router.post('/login', function(req, res) {
-  res.send('login');
-});
+router.post('/login', login);
 // /api/user/register
-router.post('/register', function(req, res) {
-  res.send('register');
-});
+router.post('/register', register);
 // /api/user/current
-router.get('/current', function(req, res) {
-  res.send('current');
-});
+router.get('/current', current);
 
 module.exports = router;
